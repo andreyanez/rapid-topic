a462c4440bed4d953a464f7c50208ce18830f2ea
andreyanez <andre.yanez22@gmail.com>
2023-02-07 15:10:25 -0500

    feat: useful topics can now be saved to localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import { Spinner } from './components/Spinner';
 
 function App() {
+	const [savedTopics, setSavedTopics] = useState<string[]>([]);
+
+	function saveTopic(newTopic: string) {
+		setSavedTopics(current => [...current, newTopic]);
+	}
+
 	async function getPost() {
 		const res = await fetch('https://www.reddit.com/r/Showerthoughts/random.json');
 		const json = await res.json();
 		return json;
 	}
+
+	useEffect(() => {
+		const topics = JSON.parse(localStorage.getItem('topics')!);
+		if (topics) setSavedTopics(topics);
+	}, []);
+
+	useEffect(() => {
+		if (savedTopics.length > 0) localStorage.setItem('topics', JSON.stringify(savedTopics));
+	}, [savedTopics]);
 
 	const {
 		isFetching,
@@ -37,6 +53,25 @@ function App() {
 			<h2>{topic[0].data.children[0].data.title}</h2>
 			{topic[0].data.children[0].data.selftext.length > 0 ? (
 				<p>{topic[0].data.children[0].data.selftext}</p>
+			) : null}
+			<div>
+				<button
+					onClick={() => {
+						saveTopic(topic[0].data.children[0].data.title);
+					}}
+				>
+					Save topic
+				</button>
+			</div>
+			{savedTopics.length ? (
+				<div>
+					<h4>Saved topics</h4>
+					<ul>
+						{savedTopics.map((topic: string, idx: number) => {
+							return <li key={idx}>{topic}</li>;
+						})}
+					</ul>
+				</div>
 			) : null}
 		</main>
 	);
