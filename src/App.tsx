dedb8def7d15aa8dcd5ee3ca27c15f845042aa29
andreyanez <andre.yanez22@gmail.com>
2023-02-09 23:28:21 -0500

    fetching logic using tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Spinner } from './components/Spinner';
 
 function App() {
-	const [topic, setTopic] = useState('');
-
-	function getPost() {
-		fetch('https://www.reddit.com/r/Showerthoughts/random.json')
-			.then(res => res.json())
-			.then(data => setTopic(data[0].data.children[0].data.title));
+	async function getPost() {
+		const res = await fetch('https://www.reddit.com/r/Showerthoughts/random.json');
+		const json = await res.json();
+		return json;
 	}
+
+	const {
+		isFetching,
+		data: topic,
+		isError,
+		refetch,
+	} = useQuery({
+		queryKey: ['topic'],
+		queryFn: getPost,
+		refetchOnWindowFocus: false,
+		enabled: false,
+	});
 
 	return (
 		<main>
-			<h1>Random convo topic</h1>
-			<button onClick={getPost}>get topic</button>
-			{topic && <p>{topic}</p>}
+			<h1>Rapid Topic</h1>
+			<button onClick={() => refetch()}>get random topic</button>
+			{topic ? <p>{topic[0].data.children[0].data.title}</p> : null}
 		</main>
 	);
 }
